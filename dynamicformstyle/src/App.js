e71c2a1ccb252b7303681e09e0ad3897537fe185
unknown <vikasdenobili@outlook.com>
2024-04-29 00:15:36 +0530

    dynamic form

---
+++ dynamicformstyle/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginSignUp from "./Components/LoginSignUp/LoginSignUp";
+
+
+const App=()=>{
+  return(
+    <div className="div_container">
+      <h3>Welcome to React</h3>
+      <LoginSignUp/>
+    </div>
+  )
+}
+
+export default App;
---
+++ dynamicformstyle/src/Components/LoginSignUp/LoginSignUp.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./LoginSignUp.css";
+import user_icon from '../Assets/person.png';
+import email_icon from '../Assets/email.png';
+import password_icon from '../Assets/password.png';
+
+const LoginSignUp = () => {
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="text">SignUp</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <img src={user_icon} alt="" />
+                    <input type="text" placeholder="Name" />
+                </div>
+                <div className="input">
+                    <img src={email_icon} alt="" />
+                    <input type="email" placeholder="Email id" />
+                </div>
+                <div className="input">
+                    <img src={password_icon} alt="" />
+                    <input type="pasword" placeholder="password"/>
+                </div>
+            </div>
+            <div className="forgot-password">Lost Password ? <span>Click here</span></div>
+            <div className="submit-container">
+                <div className="submit">Sign up</div>
+                <div className="submit">Login</div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginSignUp;
